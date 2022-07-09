34404d4a0fc4634ae9317e460337ac1f4b544717
matahariramadhan <ramadhan.matahari24@gmail.com>
2022-07-09 13:58:50 +0200

    creating ui and styling for store page

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,3 +1,18 @@
+import { Col, Row } from "react-bootstrap";
+import { StoreItem } from "../components/StoreItems";
+import storeItems from "../data/items.json";
+
 export function Store() {
-  return <h1>Store</h1>;
+  return (
+    <>
+      <h1>Store</h1>
+      <Row>
+        {storeItems.map((item) => (
+          <Col key={item.id}>
+            <StoreItem {...item} />
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
 }
