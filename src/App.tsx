971a93e85fc14c6a7e63cd808284d35009f62d7b
matahariramadhan <ramadhan.matahari24@gmail.com>
2022-07-09 13:19:43 +0200

    creating ui and styling for navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
 import { Container } from "react-bootstrap";
 import { Routes, Route } from "react-router-dom";
+import { Navbar } from "./components/Navbar";
 import { About } from "./pages/About";
 import { Home } from "./pages/Home";
 import { Store } from "./pages/Store";
 
 function App() {
   return (
-    <Container className="mb-4">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/store" element={<Store />} />
-      </Routes>
-    </Container>
+    <>
+      <Navbar />
+      <Container className="mb-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/store" element={<Store />} />
+        </Routes>
+      </Container>
+    </>
   );
 }
 
