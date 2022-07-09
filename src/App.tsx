1729271bb3867f2142a39f7216ebc38ee1722961
matahariramadhan <ramadhan.matahari24@gmail.com>
2022-07-09 10:56:27 +0200

    installing routes to different pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import { Container } from "react-bootstrap";
+import { Routes, Route } from "react-router-dom";
+import { About } from "./pages/About";
+import { Home } from "./pages/Home";
+import { Store } from "./pages/Store";
+
 function App() {
-  return <h1>Hi</h1>;
+  return (
+    <Container className="mb-4">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/store" element={<Store />} />
+      </Routes>
+    </Container>
+  );
 }
 
 export default App;
