dc49b9092d21e1df8567f48fd7dc96455b375a3d
matahariramadhan <ramadhan.matahari24@gmail.com>
2022-07-11 09:17:17 +0200

    creating ui and styling for add to cart button

--- src/components/StoreItems.tsx
+++ src/components/StoreItems.tsx
@@ -1,4 +1,4 @@
-import { Card } from "react-bootstrap";
+import { Button, Card } from "react-bootstrap";
 import { formatCurrency } from "../utilities/formatCurrency";
 
 type StoreItemProps = {
@@ -9,6 +9,8 @@ type StoreItemProps = {
 };
 
 export function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
+  const quantity = 0;
+
   return (
     <Card>
       <Card.Img
@@ -22,6 +24,27 @@ export function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
           <span className="fs-2">{name}</span>
           <span className="ms-2 text-muted">{formatCurrency(price)}</span>
         </Card.Title>
+        <div className="mt-auto">
+          {quantity === 0 ? (
+            <Button className="w-100">+ add to cart</Button>
+          ) : (
+            <div
+              className="d-flex align-items-center flex-column"
+              style={{ gap: ".5 rem" }}
+            >
+              <div className="d-flex justify-content-center">
+                <Button>-</Button>
+                <div>
+                  <span className="fs-3 ">{quantity}</span> in cart
+                </div>
+                <Button>+</Button>
+              </div>
+              <Button variant="danger" size="sm">
+                remove
+              </Button>
+            </div>
+          )}
+        </div>
       </Card.Body>
     </Card>
   );
