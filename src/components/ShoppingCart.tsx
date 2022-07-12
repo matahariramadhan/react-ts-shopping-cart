feb7a4686b479d0d45be57c97037ca4e591d8b9f
matahariramadhan <ramadhan.matahari24@gmail.com>
2022-07-12 16:37:07 +0200

    adding logic to ShoppingCart.tsx

--- src/components/ShoppingCart.tsx
+++ src/components/ShoppingCart.tsx
@@ -1,18 +1,43 @@
-import { Offcanvas, OffcanvasHeader, OffcanvasTitle } from "react-bootstrap";
+import {
+  Offcanvas,
+  OffcanvasBody,
+  OffcanvasHeader,
+  OffcanvasTitle,
+  Stack,
+} from "react-bootstrap";
 import { useShoppingCart } from "../context/ShoppingCartContext";
+import { formatCurrency } from "../utilities/formatCurrency";
+import { CartItem } from "./CartItem";
+import storeItems from "../data/items.json";
 
 type ShoppingCartProps = {
   isOpen: boolean;
 };
 
 export function ShoppingCart({ isOpen }: ShoppingCartProps) {
-  const { closeCart } = useShoppingCart();
+  const { closeCart, cartItems } = useShoppingCart();
 
   return (
     <Offcanvas show={isOpen} placement="end" onHide={closeCart}>
       <OffcanvasHeader closeButton>
         <OffcanvasTitle>Cart</OffcanvasTitle>
       </OffcanvasHeader>
+      <OffcanvasBody>
+        <Stack gap={3}>
+          {cartItems.map((item) => {
+            return <CartItem key={item.id} {...item} />;
+          })}
+        </Stack>
+      </OffcanvasBody>
+      <div className="ms-auto fw-bold fs-5">
+        Total{" "}
+        {formatCurrency(
+          cartItems.reduce((total, cartItem) => {
+            const item = storeItems.find((item) => item.id === cartItem.id);
+            return total + (item?.price || 0) * cartItem.quantity;
+          }, 0)
+        )}
+      </div>
     </Offcanvas>
   );
 }
